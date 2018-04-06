15424f86f1779fff0e9946d186677774f1f7521d
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-06 13:13:16 -0300

    joi example (to be refactored)

---
+++ joi/index.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+  orga_id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+  expirationDate: Joi.date().timestamp('unix').required(),
+  type: Joi.string().required(),
+  userAllowed: Joi.number().greater(0).less(100).integer().required(),
+  isPaid: Joi.boolean().required(),
+  nextPayment: Joi.date().timestamp('unix').required(),
+});
+
+const {error, value } = Joi.validate({ toto: "jijazdazj", orga_id: '5ac7855dc0e5e25188e3d68d', expirationDate: '12334567', type: '33', userAllowed: 99, isPaid: true, nextPayment: '1234567890' }, schema);
+
+
+
