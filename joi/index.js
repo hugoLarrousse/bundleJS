946ff5936ab1b79e54fff6925f6ccb56e5323c38
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-06 16:17:04 -0300

    add comments

--- joi/index.js
+++ joi/index.js
@@ -1,3 +1,7 @@
+// Usefull when you want to insert shema and check for the field
+// could be improve for update!!
+
+
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
@@ -12,4 +16,10 @@ const schema = Joi.object().keys({
 const {error, value } = Joi.validate({ toto: "jijazdazj", orga_id: '5ac7855dc0e5e25188e3d68d', expirationDate: '12334567', type: '33', userAllowed: 99, isPaid: true, nextPayment: '1234567890' }, schema);
 
 
+
+// Be careful! date timestamp is not really good for timestamp....
+
+// by default other field will throw an error
+
+// regex cool but don't check is the type is ObjectID!!
 
