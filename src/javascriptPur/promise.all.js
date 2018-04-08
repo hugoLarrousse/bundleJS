fe2b6a27b9e07fe3f0e54b83345a1dca793dd2c8
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-08 12:08:24 -0300

    promise.all hot it works!!

---
+++ src/javascriptPur/promise.all.js
@@ -0,0 +1,24 @@
+const timeoutLOL = async (t) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, t, 'lol');
+  });
+};
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = () => new Promise((resolve, reject) => {
+  resolve(timeoutLOL(2000));
+});
+const promise4 = async () => {
+  await timeoutLOL(3000);
+};
+
+const test = async () => {
+  console.time('TIME')
+  Promise.all([promise1, promise2, promise3(), promise4()]).then((values) => {
+    console.timeEnd('TIME');
+    console.log('values :', values);
+  });
+};
+
+test();
