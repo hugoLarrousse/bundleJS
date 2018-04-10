936b327ef9a6fcfc9166966d4c78ccc04065f490
Hugo Larrousse <hugo.larrousse@chance.co>
2018-04-10 12:06:25 -0300

    add helper mongo (remove underscore  & sanitize)

--- src/mongo/helper.js
+++ src/mongo/helper.js
@@ -15,6 +15,31 @@ const softDeleteRetrieveCondition = { // we should implement it soon
   deletedAt: null,
 };
 
+export const renameIdToUnderscoreId = (model) => {
+  if (model._id) {
+    return model;
+  }
+
+  if (!model.id) {
+    const err = new Error('Model has no id');
+    throwError(__filename, renameIdToUnderscoreId.name, err.message, err);
+  }
+
+  const { id, ...rest } = model;
+  return {
+    _id: mongo.ObjectId(id),
+    ...rest,
+  };
+};
+
+export const sanitize = (doc) => {
+  const { _id, deletedAt, ...rest } = doc;
+  return {
+    id: _id,
+    ...rest,
+  };
+};
+
 exports.timeNow = timeNow;
 exports.addUpdatedAtToModel = addUpdatedAtToModel;
 exports.addCreatedAtToModel = addCreatedAtToModel;
